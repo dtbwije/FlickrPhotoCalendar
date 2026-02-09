abb39df88ee9ea907e2f6cccd271705e832288f2
dtbwi <dtbwije@gmail.com>
2026-02-10 01:18:08 +0200

    creating the new component to redirect autheticator

---
+++ README.md
@@ -0,0 +1,10 @@
+Flickr photo calendar
+--
+
+Run front end
+./gradle start
+
+Run backend
+./gradle bootRun
+
+
--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {app-flickr-auth-redirect} from
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
